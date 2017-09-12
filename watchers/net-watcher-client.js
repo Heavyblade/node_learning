c205522fbb451fdcb1b78d234a28fc1098b49b7e
CristianV <cristianvg2003@gmail.com>
2017-09-12 15:12:35 -0500

    Adding handler to buffer incomming messages

--- watchers/net-watcher-client.js
+++ watchers/net-watcher-client.js
@@ -1,15 +1,15 @@
 'use strict';
 const net = require('net');
 const client = net.connect({port: 60300});
+const ldjClient = require("./lib/ldj-client").connect(client);
 
-client.on('data', data => {
-  const message = JSON.parse(data);
-  if (message.type === 'watching') {
-    console.log(`Now watching: ${message.file}`);
-  } else if (message.type === 'changed') {
-    const date = new Date(message.timestamp);
-    console.log(`File changed: ${date}`);
-  } else {
-    console.log(`Unrecognized message type: ${message.type}`);
-  }
+ldjClient.on("message", message => {
+    if (message.type === 'watching') {
+      console.log(`Now watching: ${message.file}`);
+    } else if (message.type === 'changed') {
+      const date = new Date(message.timestamp);
+      console.log(`File changed: ${date}`);
+    } else {
+      console.log(`Unrecognized message type: ${message.type}`);
+    }
 });
